3dd4e5a59de06dd4254cb1b10d2ece4039dd82c2
Raul Craciun <raul.carciun@students.codeinstitute.net>
2024-10-09 15:47:04 +0000

    Add code to check the answer and respond tothe user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                 checkAnswer();
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -39,8 +39,8 @@ function runGame(gameType) {
 }
 
 /**
- * Checks the andswer against the first element in
- * the returnet calculateCorrectAnswer array
+ * Check the answer against the first element in
+ * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
 
@@ -51,10 +51,11 @@ function checkAnswer() {
     if (isCorrect) {
         alert("Hey! You got it right! :D");
     } else {
-        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
     }
 
     runGame(calculatedAnswer[1]);
+
 }
 
 /**
@@ -62,16 +63,18 @@ function checkAnswer() {
  * directly from the dom, and returns the correct answer.
  */
 function calculateCorrectAnswer() {
+
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById('operator').innerText;
+    let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else {
         alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting`;
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
+
 }
 
 function incrementScore() {
